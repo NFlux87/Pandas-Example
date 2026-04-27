fb823fa6e5a47ffa7047c36d94df3c0507cee035
Nflux <>
2026-04-27 14:50:24 +0700

    Add new dataset pokemon.csv and Add exmaple of import csv files on file importcsv.py

---
+++ src/importcsv.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# import from csv file
+df = pd.read_csv(filepath_or_buffer= 'data/pokemons.csv')
+
+# # default will print 5 first data and last 5 data
+# print(df)
+
+# to_sting make it print all data
+# if you're looking for column only you can you
+# df['column_name']
+# df.loc['row_name']
